606cd2df5883bf332ac7f9f5bbabe84d181d9bda
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-12 20:58:43 -0300

    chapter39 return userIten

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,4 +1,16 @@
 import React from 'react' 
 import './UsersList.css'
-const UsersList = props =>{ }
+import UserItem from './UserItem'
+const UsersList = props =>{ 
+    if (props.items.lengh === 0) {
+        return <div className="center">
+            <h2>No users found</h2>
+        </div>
+    }
+    return <ul>
+        {props.items.map(user=>{
+            return <UserItem/>
+        })}
+    </ul>
+}
 export default UsersList 
