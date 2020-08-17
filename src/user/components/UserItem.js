06fcc6f347b02b8d5833e866fbfa183912dbcf74
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-17 20:35:28 -0300

    sec5cap40_addingUserItem

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -2,7 +2,13 @@ import React from 'react'
 import './UserItem.css' 
 const UsersItem = props =>{
     return (
-        <li className="user-item"></li>
+        <li className="user-item">
+            <div className="user-item__content">
+                <div className="user-item__image">
+                    <img src={props.image} alt={props.name}/>
+                </div>
+            </div>
+        </li>
     )
  }
 export default UsersItem 
