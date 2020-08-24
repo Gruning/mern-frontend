2a858ff8a98fb356dcba2086a159cb02d00bb750
Alberto Gruning <alberto.gruning@ndvsistemas.com>
2020-08-24 19:39:05 -0300

    sec5cap39_addingAUserList_fixErrorInMock

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import UsersList from '../components/UsersList'
 const Users= ()=>{
-    const USERS = {
+    const USERS =[{
         id:'u1',
         name:'Alberto Grunig',
-        image:'https://images.pexels.com/photos/839011/pexels-photo-839011.jpg?auto=compress',
+        image:'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
         places:3
-    }
+    }]
     return <UsersList items={USERS}/>;
 }
 export default Users
