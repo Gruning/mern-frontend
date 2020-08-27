e7cd199609bfb292d16f03b1c24a6a7f4bd380a8
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-08-26 21:09:37 -0300

    test runing server ok

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,7 +4,7 @@ const Users= ()=>{
     const USERS =[{
         id:'u1',
         name:'Alberto Grunig',
-        image:'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        image:'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=75&w=126',
         places:3
     }]
     return <UsersList items={USERS}/>;
