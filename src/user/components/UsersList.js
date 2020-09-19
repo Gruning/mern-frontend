d44e5a75400fe067783afc8d30cb7d3ac5b7c33f
Alberto Gruning <alberto.gruning.zen@gmail.com>
2020-09-19 19:51:26 -0300

    sec5cap40_adding

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,22 +1,26 @@
 import React from 'react' 
 import './UsersList.css'
 import UserItem from './UserItem'
-const UsersList = props =>{ 
+const UsersList = props =>{  
+    console.log(props)
+    console.log(props.items.lengh)
     if (props.items.lengh === 0) {
-        return <div className="center">
+        return (<div className="center">
             <h2>No users found</h2>
-        </div>
+        </div>)
     }
-    return <ul className="users-list">
+    return (
+    <ul className="users-list">
              {props.items.map(user=>(
              <UserItem 
-            key= { user.id } 
-            id={ user.id } 
-            image={ user.image }
-            name={user.name} 
-            placeCount= {user.places}
-            />
-        ))}
+                key= { user.id } 
+                id={ user.id } 
+                image={ user.image }
+                name={user.name} 
+                placeCount= {user.places}
+                />
+            ))}
     </ul>
+    )
 }
 export default UsersList 
