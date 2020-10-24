45ab480f36b12c1f1b6a2a1d7626ba1692463095
AlbertoGruning <alberto.gruning.zen@gmail.com>
2020-10-24 14:01:18 -0300

    sec5cap49-renderinUserPlaces

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import PlaceList from '../components/PlaceList'
 
+const DUMMY_PLACES =[
+        {
+            id:'p1',
+            title:'Empire State Building',
+            description:'a famous Building',
+            imageUrl:'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+            address:'20 W 34th St, New York, NY 10001, United States',
+            location:{
+                lat:40.7484405,
+                long:-73.9878531,
+            },
+            creator:'u1',
+        },
+        {
+            id:'p1',
+            title:'Empire State Building',
+            description:'a famous Building',
+            imageUrl:'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
+            address:'20 W 34th St, New York, NY 10001, United States',
+            location:{
+                lat:40.7484405,
+                long:-73.9878531,
+            },
+            creator:'u2',
+        },
+    ]
 const UserPlaces=()=>{
-    return
+    return <PlaceList items={DUMMY_PLACES}/>
 }
 
 export default UserPlaces
