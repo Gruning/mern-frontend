d6865b2eb2f7f0518fa4a67c1a1cd2f017c8c539
AlbertoGruning <alberto.gruning.zen@gmail.com>
2021-06-01 20:42:23 -0300

    cap154-imports

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { useParams } from "react-router-dom"
-
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import PlaceList from '../components/PlaceList'
 import { useHttpClient } from '../../shared/hooks/http-hook'
 
             //'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
 const UserPlaces=()=>{
+    const [loadedPlaces,setLoadedPlaces]= useState()
     const {isLoading, error, sendRequest,clearError}= useHttpClient()
     const userId = useParams().userId
-    const loadedPlaces = {}// DUMMY_PLACES.filter(place=>place.creator === userId)
+    useEffect(()=>{
+        const fetchPlaces = async () =>{
+            try {
+                const responseData = await sendRequest(`http://localhost:5000/api/places/user/${userId}`)
+                setLoadedPlaces(responseData.places)
+
+            } catch (err) {}
+        }
+        fetchPlaces()
+    },[sendRequest, userId])
     return <PlaceList items={loadedPlaces}/>
 }
 
