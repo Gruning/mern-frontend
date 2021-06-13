4ec2c266d96ef1c57a3fe339da6784046bfac7af
AlbertoGruning-Arch <alberto.gruning.zen@gmail.com>
2021-06-13 19:45:27 -0300

    feat: :sparkles: cap156-ok
    delete place
    
    cap 156 ok

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -5,10 +5,13 @@ import Button  from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal"
 import Map from '../../shared/components/UIElements/Map'
 import {AuthContext} from '../../shared/context/auth-context'
-
+import ErrorModal from '../../shared/components/UIElements/ErrorModal'
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
+import {useHttpClient} from '../../shared/hooks/http-hook'
 import './PlaceItem.css' 
 
 const PlaceItem = props =>{
+    const {isLoading, error, sendRequest,clearError}= useHttpClient()
     const auth = useContext(AuthContext)
     const [showMap,setShowMap] = useState(false)
     const[showConfirmModal,setShowConfirmModal]= useState(false)
@@ -20,14 +23,21 @@ const PlaceItem = props =>{
     const cancelDeleteHandler =()=>{
         setShowConfirmModal(false)
     }
-    const confirmDeleteHandler = ()=>{
+    const confirmDeleteHandler =async ()=>{
         setShowConfirmModal(false)
-        console.log('deleting...')
+        try{
+        await sendRequest(
+            `http://localhost:5000/api/places/${props.id}`,
+            'DELETE'
+            )
+            props.onDelete(props.id)
+        } catch (err) {}
     }
 
 
     return (
         <React.Fragment>
+            <ErrorModal error={error} onClear={clearError}/>
             <Modal
                 show={showMap}
                 onCancel={closeMapHandler}
@@ -55,6 +65,7 @@ const PlaceItem = props =>{
             </Modal>
             <li className="place-item">
                 <Card className="place-item__content">
+                    {isLoading && <LoadingSpinner asOverlay/>}
                     <div className="place-item__image">
                         <img src={props.image} alt={props.title} />
                     </div>
