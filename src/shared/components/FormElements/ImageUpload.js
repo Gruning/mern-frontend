18701d761158afd5dc63215b20713ba36095d191
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-06-19 16:13:53 -0400

    cap161-jsx

--- src/shared/components/FormElements/ImageUpload.js
+++ src/shared/components/FormElements/ImageUpload.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import './imageUpload'
+import React ,{useRef} from 'react'
+import Button from './Button'
+import './imageUpload.css'
 
 const imageUpload = props => {
+    const pickImageHandler =() => {}
+const filePickerRef = useRef()
     return <div className="form-control">
-        <input id ={props.id} style={{display:none}} type="file" accept=".jpg" />
+        <input 
+        id ={props.id} 
+        style={{display:none}} 
+        type="file" 
+        accept=".jpg,.png,.jpeg" 
+        />
+        <div className={`image-upload ${props.center && 'center'}`} >
+            <div className='image-upload__preview'>
+                <img src='' alt='Preview'/>
+            </div>
+            <Button type='button' onClick={pickImageHandler}>Pick Image</Button>
+        </div>
     </div>
 }
 export default imageUpload
