ac80ee80de8665c6800740582612f6bd09a85c66
AlbertoGruning-Fedora <alberto.gruning.zen@gmail.com>
2021-06-21 15:45:59 -0300

    cap165

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -58,16 +58,16 @@ const Auth = ()=>{
         
         if(isLoginMode){
             try {
+                const formData = new FormData()
+                formData.append('email',formState.inputs.email.value)
+                formData.append('name',formState.inputs.name.value)
+                formData.append('password',formState.inputs.password.value)
+                formData.append('image',formState.inputs.image.value)
+                
                 const responseData = await sendRequest(
                     'http://localhost:5000/api/users/login',
                     'POST',
-                    JSON.stringify({
-                        email: formState.inputs.email.value,
-                        password: formState.inputs.password.value
-                    }),
-                    {
-                        'Content-Type':'application/Json'
-                    }
+                    formData
                 )
                 auth.login(responseData.user.id)
             } catch (err) {}
